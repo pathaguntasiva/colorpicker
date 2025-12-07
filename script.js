c8716269ee7a63b2f56588cee2794e34bf0a8b2a
pathaguntasiva <pathaguntasiva@gmail.com>
2025-12-07 22:37:14 +0530

    developed color picker

---
+++ script.js
@@ -0,0 +1,29 @@
+function changeTextColor(){
+    let h2=document.getElementById('head');
+    h2.style.color="red";
+}
+
+function changeTextColorY(){
+    let h2=document.getElementById('head');
+    h2.style.color="yellow";
+}
+
+function changeTextColorG(){
+    let h2=document.getElementById('head');
+    h2.style.color="green";
+}
+
+function changeBackgroundColor(){
+    let h2=document.getElementById('head');
+    h2.style.backgroundColor="red";
+}
+
+function changeBackgroundColorY(){
+    let h2=document.getElementById('head');
+    h2.style.backgroundColor="yellow";
+}
+
+function changeBackgroundColorG(){
+    let h2=document.getElementById('head');
+    h2.style.backgroundColor="green";
+}
